177d0ad9741f82d07b4e5be9115b28bcf5173f7d
M8A98 <mauro8a_10@hotmail.com>
2018-12-13 10:48:44 -0600

    Almost there

--- colorGame.js
+++ colorGame.js
@@ -1,1 +1,46 @@
+var colors = [
+	"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)",
+]
 
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
+
+
+var squares = document.querySelectorAll(".square");
+
+for (var i = 0; i < squares.length; i++){
+	//add initial colors to squares
+	squares[i].style.backgroundColor = colors[i];
+	//add click Listeners
+	squares[i].addEventListener("click", function(){
+		//Grab color of square
+		var clickedColor = this.style.backgroundColor;
+		//Compare color to picked color
+		if (clickedColor == pickedColor) {
+			messageDisplay.textContent = "Correct";
+			changeColors(pickedColor);
+		}else{
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try again";
+		}
+	});
+}
+
+function changeColors(color){
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
